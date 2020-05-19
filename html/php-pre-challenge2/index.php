aee6d0f386bace830bb96365599a526ab56a6f7c
39nina <62025642+39nina@users.noreply.github.com>
2020-05-19 11:49:11 +0900

    PHP練習課題2の初回提出いたします。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,8 +2,17 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+// (配列の中身の数-1)回ソートを繰り返す
+for ($i = 1; $i < count($array); $i++) {
+    // index番号0から、最後から１つ手前のindex番号まで1ずつ増やす（比較対象を移動）
+    for ($index = 0; $index < (count($array)-1); $index++) {
+        // 右隣の数字と比較してそれより大きい場合のみ、配列の中身を入れ替えて上書き
+        if ($array[$index] > $array[$index+1]) {
+            $temporaryIndex = $array[$index];
+            $array[$index] = $array[$index+1];
+            $array[$index+1] = $temporaryIndex;
+        }
+    }
 }
 // 修正はここまで
 
