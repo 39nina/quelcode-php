d38d9034fddc69d9c0dc4e14d06526b360b05794
39nina <62025642+39nina@users.noreply.github.com>
2020-05-19 16:13:51 +0900

    最初のfor文をデクリメントに変更。次のfor文で、確定済の値を除外し比較範囲を毎周減らすよう変更。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,10 +3,14 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 // バブルソートの考え方は(https://www.youtube.com/watch?v=Hy5DhsGYykw)を参考にしました。
+
+$listCount = count($array);
+$last = $listCount - 1; 
 // (配列の中身の数-1)回ソートを繰り返す
-for ($i = 1; $i < count($array); $i++) {
-    // index番号0から、最後から１つ手前のindex番号まで1ずつ増やす（比較対象を移動）
-    for ($index = 0; $index < (count($array)-1); $index++) {
+for ($i = $last; $i > 0; $i--) {
+    // index番号0から、最後から1つ手前($last)のindex番号まで1つずつ増やす。
+    // その際、$lastは1周毎に1つずつ左にずれ、比較範囲を減らしている。
+    for ($index = 0; $index < $last; $index++) {
         // 右隣の数字と比較してそれより大きい場合のみ、配列の中身を入れ替えて上書き
         if ($array[$index] > $array[$index+1]) {
             $temporaryIndex = $array[$index];
