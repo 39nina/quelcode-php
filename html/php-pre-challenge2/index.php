78599b56826f18f8cd4604b329b44ef5b3a49687
39nina <62025642+39nina@users.noreply.github.com>
2020-05-20 14:47:55 +0900

    コメントの文言修正、変数($listCount, $last)の$iへの統合

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,15 +4,13 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 // バブルソートの考え方は(https://www.youtube.com/watch?v=Hy5DhsGYykw)を参考にしました。
 
-$listCount = count($array);
-$last = $listCount - 1; 
+$i = count($array) - 1;
 // (配列の中身の数-1)回ソートを繰り返す
-for ($i = $last; $i > 0; $i--) {
-    $last = $i;
-    // index番号0から、最後から1つ手前($last)のindex番号まで1つずつ増やす。
-    // その際、$lastは1周毎に1つずつ左にずれ、比較範囲を減らしている。
-    for ($index = 0; $index < $last; $index++) {
-        // 右隣の数字と比較してそれより大きい場合のみ、配列の中身を入れ替えて上書き
+for ($i; $i > 0; $i--) {
+    // インデックス番号0から、最後から1つ手前のインデックス番号まで1つずつ増やす
+    // その際、$iは配列最後の数字を1周毎に１つずつ範囲対象外としている
+    for ($index = 0; $index < $i; $index++) {
+        // 次のインデックス番号の中身と比較し、自身の方が大きい場合のみ配列の中身を入れ替えて上書きする
         if ($array[$index] > $array[$index + 1]) {
             $temporaryIndex = $array[$index];
             $array[$index] = $array[$index + 1];
