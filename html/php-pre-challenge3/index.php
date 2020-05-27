6469b2f1f8bf78aa6c5c0af492ec76c6a15462a5
39nina <62025642+39nina@users.noreply.github.com>
2020-05-27 16:02:42 +0900

    各変数名の変更

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -27,22 +27,22 @@ try {
 }
 
 // DBの値の取り出し
-$records = $db->query('SELECT * FROM prechallenge3');
-$record = array_map('intval', $records->fetchAll(PDO::FETCH_COLUMN));
+$statement = $db->query('SELECT * FROM prechallenge3');
+$records = array_map('intval', $statement->fetchAll(PDO::FETCH_COLUMN));
 
 // $array:配列全体、$pick:組み合わせる値の数として全ての組み合わせを返すファンクションcombinationを作成
 // 参考にしたページ（https://stabucky.com/wp/archives/2188）
 function combination ($array, $pick) {
-    $sum = count($array);
-    if($sum < $pick) {   // 組み合わせにつかう数が配列の総数を超える場合、成立しないためif文の処理を終了
+    $count = count($array);
+    if($count < $pick) {   // 組み合わせにつかう数が配列の総数を超える場合、成立しないためif文の処理を終了
         return;
     } elseif($pick === 1) {  // 配列$arrayのうち単独の値で$limitとの一致を確認する場合、その値を$arrays[$i]に代入
-        for($i = 0; $i < $sum; $i++) {
+        for($i = 0; $i < $count; $i++) {
         $arrays[$i] = array($array[$i]);
         }
     } elseif($pick > 1) {  // 配列$arrayのうち複数の値を組み合わせて$limitとの一致を確認する場合、組み合わせの１項目めを固定とするため一度除外して２項目め以降で組み合わせを作る。その後、１項目めを組み合わせ配列$otherPartの頭に挿入し、$arrays[$j]に代入（再帰処理される）
         $j = 0;
-        for($i = 0; $i < $sum - $pick + 1; $i++) {
+        for($i = 0; $i < $count - $pick + 1; $i++) {
             $otherPart = combination(array_slice($array, $i + 1), $pick - 1);
             foreach($otherPart as $op) {
                 array_unshift($op, $array[$i]);
@@ -55,10 +55,10 @@ function combination ($array, $pick) {
 }
 
 // combinationファンクションを使い、全組み合わせから$limitと一致するものを配列$combiに代入
-$sumCount = count($record);
+$count = count($records);
 $combi = [];
-for($i = 1; $i < ($sumCount + 1); $i++) {
-    $temps = combination($record, $i);
+for($i = 1; $i < ($count + 1); $i++) {
+    $temps = combination($records, $i);
     foreach($temps as $temp) {
         if(array_sum($temp) === $limit) {
             array_push($combi, $temp);
@@ -67,5 +67,5 @@ for($i = 1; $i < ($sumCount + 1); $i++) {
 }
 
 // json形式で出力
-$json_combi = json_encode($combi);
-print($json_combi);
+$jsonCombi = json_encode($combi);
+print($jsonCombi);
