13e028e57038772f37f414e4ffb49db5ef58460f
39nina <62025642+39nina@users.noreply.github.com>
2020-06-10 00:25:52 +0900

    いいねの数判定を修正

--- html/php-challenge/fav.php
+++ html/php-challenge/fav.php
@@ -2,26 +2,29 @@
 session_start();
 require('dbconnect.php');
 
-$favs = $db->prepare('SELECT * FROM fav WHERE member_id=? AND post_id=?');
-$favs->execute(array($_SESSION['id'],$_REQUEST['id']));
-$fav = $favs->fetch();
-if ($fav['fav_flg'] != '') {
+$st = $db->prepare('SELECT * FROM posts WHERE id=?');
+$st->execute(array($_REQUEST['id']));
+$post = $st->fetch();
+
+// 押下するいいねボタンの元ツイートのidを取得
+$st = $db->prepare('SELECT * FROM posts WHERE id=?');
+$st->execute(array($post['id']));
+$orgid = (int)$st->fetch()['original_post_id'];
+
+// 押下するいいねボタンの元ツイートをいいね済か否か確認
+$st = $db->prepare('SELECT * FROM fav f, posts p WHERE f.post_id=p.original_post_id AND f.member_id=? AND p.id=? AND p.id=p.original_post_id');
+$st->execute(array($_SESSION['id'], $orgid));
+$fav = $st->fetch();
+if ($fav['fav_flg'] !== NULL) {
 	$flg = (int)($fav['fav_flg']);
 }
 
-$st = $db->prepare('SELECT * FROM fav f, posts p WHERE f.post_id=p.id AND f.post_id=? AND f.member_id=?');
-$st->execute(array($_REQUEST['id'], $_SESSION['id']));
-$check = $st->fetch();
-
-if ($flg === 0) {
-	$idset = $db->prepare('UPDATE fav SET fav_flg=1 WHERE fav.post_id=posts.id AND fav.post_id=? AND fav.member_id=?');
-	$idset->execute(array($check['original_post_id'],$_SESSION['id']));
-} elseif ($flg === 1) {
-	$idset = $db->prepare('UPDATE fav SET fav_flg=0 WHERE post_id=? AND member_id=?');
-	$idset->execute(array($_REQUEST['id'], $_SESSION['id']));
+if ($flg === 1) {
+	$st = $db->prepare('DELETE FROM fav WHERE post_id=? AND member_id=?');
+	$st->execute(array($orgid, $_SESSION['id']));
 } elseif ($flg === NULL) {
-	$insert = $db->prepare('INSERT INTO fav SET post_id=?, member_id=?, fav_flg=1');
-	$insert->execute(array($check['original_post_id'], $_REQUEST['id'], $_SESSION['id']));
+	$st = $db->prepare('INSERT INTO fav SET post_id=?, member_id=?, fav_flg=1');
+	$st->execute(array($orgid, $_SESSION['id']));
 }
 
 header('Location: index.php'); exit();
